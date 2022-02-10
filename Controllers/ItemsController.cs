6db0516d734927b49089abf5d62650e93f3dbb64
Mario Camelo <mariuo@gmail.com>
2022-02-10 09:28:43 -0500

    projet created

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,42 @@
+﻿using Catalog.Dtos;
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+namespace Catalog.Controllers
+
+{
+    //Get /items
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IItemsRepository repository;
+
+        public ItemsController(IItemsRepository repository)
+        {
+            this.repository = repository;
+
+        }
+        // GET /items
+        [HttpGet]
+        public IEnumerable<ItemDto> GetItems()
+        {
+            var items = repository.GetItems().Select(item => item.AsDto());
+            return items;
+        }
+        // GET /items/{id}
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+            if (item is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item.AsDto());
+        }
+
+    }
+}
---
+++ Repositories/IItemsRepository.cs
@@ -0,0 +1,10 @@
+﻿using Catalog.Entities;
+
+namespace Catalog.Repositories
+{
+    public interface IItemsRepository
+    {
+        Item GetItem(Guid id);
+        IEnumerable<Item> GetItems();
+    }
+}
