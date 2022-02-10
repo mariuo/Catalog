125d2cb817820fc9320b56bb53ba5c71e9182a72
Mario Camelo <mariuo@gmail.com>
2022-02-10 10:36:56 -0500

    PUT and Delete

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -37,6 +37,7 @@ namespace Catalog.Controllers
 
             return Ok(item.AsDto());
         }
+        // POST /items
         [HttpPost]
         public ActionResult<ItemDto> CreateItem(CreateItemDto itemDto)
         {
@@ -51,6 +52,37 @@ namespace Catalog.Controllers
             return CreatedAtAction(nameof(GetItem), new { id = item.Id }, item.AsDto());
         }
 
+        //PUT /items/{id}
+        [HttpPut("{id}")]
+        public ActionResult UpdateItem(Guid id, UpdateItemDto itemDto)
+        {
+            var existingItem = repository.GetItem(id);
+            if(existingItem is null)
+            {
+                return NotFound();
+            }
+            Item updatedItem = existingItem with
+            {
+                Name = itemDto.Name,
+                Price = itemDto.Price,
+            };
 
+            repository.UpdateItem(updatedItem);
+
+            return NoContent();
+        }
+        //DELETE /items/{id}
+        [HttpDelete("{id}")]
+        public ActionResult DeleteItem(Guid id)
+        {
+            var existingItem = repository.GetItem(id);
+            if (existingItem is null)
+            {
+                return NotFound();
+            }
+            repository.DeleteItem(id);
+
+            return NoContent();
+        }
     }
 }
