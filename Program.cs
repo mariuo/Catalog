bda5e1776abfaa360e298e8414da8acac05756a5
Mario Camelo <mariuo@gmail.com>
2022-02-11 12:29:11 -0500

    Change to async Task MongoDb

--- Program.cs
+++ Program.cs
@@ -17,7 +17,10 @@ builder.Services.AddSingleton<IMongoClient>(serviceProvider =>
     return new MongoClient(settings.ConnectionString);
 });
 builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    options.SuppressAsyncSuffixInActionNames = false;
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
